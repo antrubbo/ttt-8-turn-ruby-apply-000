1ce27bfe68fc76d339ac44eb36f4b4459827bc45
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-08-31 19:59:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,12 +37,12 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   input_to_index(user_input)
-  # if 
-  #   valid_move?(board, index) == true
-  #   turn(board)
-  # else
-  #   false
-  # end
+  if 
+    valid_move?(board, index) == true
+    turn(board)
+  else
+    false
+  end
 end
 
 
